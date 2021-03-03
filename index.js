50061b6ef08bcad1dc9caece3dcb087404b76971
Cassie Jones <cassie.jones9@gmail.com>
2021-03-03 13:24:37 -0800

    new app setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+.get("/math", (req, res) => {
+  var result = solveMath(req);
+  res.render("pages/results", { answer: result });
+});
+
+
+
+listen(PORT, () => console.log(`Listening on ${PORT}`))
+
