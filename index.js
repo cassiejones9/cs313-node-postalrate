de5c9edcbd29bae19cfeed9c8e32f90c59215ea2
Cassie Jones <cassie.jones9@gmail.com>
2021-03-03 17:04:07 -0800

    setup

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8888
+const app = express()
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/postal', (req, res) => res.render('pages/index'))
+
+// app.get("/math", (req, res) => {
+//   var result = solveMath(req);
+//   res.render("pages/results", { answer: result });
+// });
 
 
-.get("/math", (req, res) => {
-  var result = solveMath(req);
-  res.render("pages/results", { answer: result });
-});
 
-
-
-listen(PORT, () => console.log(`Listening on ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
