2a3b65c60075a06586e3f9881d6b4b136db5f91e
Cassie Jones <cassie.jones9@gmail.com>
2021-03-04 10:34:04 -0800

    setup

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 8888
+const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 app.get('/postal', (req, res) => res.render('pages/index'))
 
 // app.get("/math", (req, res) => {
@@ -17,5 +16,4 @@ app.get('/postal', (req, res) => res.render('pages/index'))
 
 
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
-
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
